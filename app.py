8b80c045e129aa93510a5b03f57f9b135ea37bca
jcunningham3 <jcunningham3@gmail.com>
2021-05-18 18:07:18 -0500

    add heroku postgres url

--- app.py
+++ app.py
@@ -5,13 +5,10 @@ from forms import UserForm, TweetForm
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Tottenham7@localhost/auth_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATBASE_URI', 'postgresql://postgres:Tottenham7@localhost/auth_db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','secret1' )
-print('********************************************')
-print(app.config["SECRET_KEY"])
-print('********************************************')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
